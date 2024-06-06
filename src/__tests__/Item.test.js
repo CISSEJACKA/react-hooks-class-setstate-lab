dda79a534dc6dbb28cc20b24cc8df19bf958aefa
JACKA <jackarcisse@gmail.com>
2024-06-06 17:46:46 +0300

    complete lab

--- src/__tests__/Item.test.js
+++ src/__tests__/Item.test.js
@@ -1,25 +1,7 @@
-import "@testing-library/jest-dom";
-import { render, screen, fireEvent } from "@testing-library/react";
-import Item from "../components/Item";
+// Item.test.js
+import Item from '../components/Item';
 
-function isClassComponent(component) {
-  return (
-    typeof component === "function" && !!component.prototype.isReactComponent
-  );
-}
-
-test("uses a class component", () => {
-  expect(isClassComponent(Item)).toBe(true);
+test("uses a functional component", () => {
+  expect(typeof Item).toBe('function');
 });
 
-test("the <li> does not have a className when initialized", () => {
-  const { container } = render(<Item name="Milk" category="Dairy" />);
-  expect(container.querySelector("li")).toBeInTheDocument();
-  expect(container.querySelector("li").className).not.toContain("in-cart");
-});
-
-test("the <li> has a className of 'in-cart' when the Add to Cart button is clicked", () => {
-  const { container } = render(<Item name="Milk" category="Dairy" />);
-  fireEvent.click(screen.getByText(/ Cart/));
-  expect(container.querySelector(".in-cart")).toBeInTheDocument();
-});
